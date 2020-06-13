76829a4754b4ed826f7f1fe5f613051343e5d144
Cao Minh Duc <devs@Caos-MacPro.local>
2020-06-13 10:28:38 +0700

    title smaller

--- resources/views/basic.blade.php
+++ resources/views/basic.blade.php
@@ -1,6 +1,6 @@
 <div class="mx-1 bg-white shadow sm:rounded-lg {{ $size }}">
     <div class="container flex flex-col h-full p-5 mx-auto">
-        <h1 class="mb-3 text-2xl border-b">{{ $title }}</h1>
+        <h1 class="mb-3 text-xl border-b">{{ $title }}</h1>
         {{ $slot }}
     </div>
 </div>
