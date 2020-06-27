73536b7523405bf0175804400f5e2bf945f85273
Cao Minh Duc <devs@Caos-MacPro.local>
2020-06-27 09:04:41 +0700

    refactor padding

--- resources/views/basic.blade.php
+++ resources/views/basic.blade.php
@@ -1,6 +1,8 @@
-<div class="mx-1 bg-white shadow sm:rounded-lg {{ $size }}">
-    <div class="container flex flex-col h-full p-5 mx-auto">
-        <h1 class="mb-3 text-xl border-b">{{ $title }}</h1>
-        {{ $slot }}
+<div class="p-1 {{ $size }}">
+    <div class="bg-white shadow sm:rounded-lg">
+        <div class="container flex flex-col h-full p-5 mx-auto">
+            <h1 class="mb-3 text-xl border-b">{{ $title }}</h1>
+            {{ $slot }}
+        </div>
     </div>
 </div>
